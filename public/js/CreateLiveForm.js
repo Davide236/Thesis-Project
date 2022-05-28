32508d224cc06721864f76bf432e0d3935b1ba4b
Davide236 <rigodavi99@gmail.com>
2022-05-28 17:07:32 +0200

    Camera changes

--- public/js/CreateLiveForm.js
+++ public/js/CreateLiveForm.js
@@ -1,17 +1,5 @@
 //Get a list of all the connected video devices so that the user can choose which one to use
 window.onload = async function() {
-    getPermission().then(
-        async (_message) => {
-            await showDevices();
-        },
-        (_message) => {
-            alert("Couldn't get permission to access camera");
-        }
-    )
-};
-
-//Function which shows the available devices to the user
-async function showDevices() {
     $('#videoDevice').html('');
     let devices = await getVideoDevices();
     videoDeviceHTML = '';
@@ -21,25 +9,7 @@ async function showDevices() {
         `;
     }
     $('#videoDevice').append(videoDeviceHTML);
-}
-
-//Ask the users for permission to access video and audio devices
-async function getPermission() {
-    navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-    })
-    //Start and immediately stop the stream
-    .then(function(stream) {
-        stream.getTracks()[0].stop();
-        stream.getTracks()[1].stop(); 
-        return Promise.resolve("Success");
-    })
-    .catch(function(_err) {
-        //Error
-        return Promise.resolve("Error");
-    });
-}
+};
 
 //Get the list of connected video devices to display to the user
 async function getVideoDevices() {
--- public/js/Homepage.js
+++ public/js/Homepage.js
@@ -13,3 +13,22 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+//Ask the users for permission to access video and audio devices
+async function askPermission() {
+  navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+  })
+  //Start and immediately stop the stream
+  .then(function(stream) {
+      stream.getTracks()[0].stop();
+      stream.getTracks()[1].stop(); 
+      return Promise.resolve("Success");
+  })
+  .catch(function(_err) {
+      //Error
+      return Promise.resolve("Error");
+  });
+}
