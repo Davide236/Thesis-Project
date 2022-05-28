28cd19046dafaf5bf47228decb87ca0373110dfa
Davide236 <rigodavi99@gmail.com>
2022-05-28 16:44:38 +0200

    Changing video permission

--- public/js/CreateLiveForm.js
+++ public/js/CreateLiveForm.js
@@ -1,6 +1,7 @@
 //Get a list of all the connected video devices so that the user can choose which one to use
 window.onload = async function() {
     $('#videoDevice').html('');
+    await getPermission();
     let devices = await getVideoDevices();
     console.log(devices);
     videoDeviceHTML = '';
@@ -12,7 +13,24 @@ window.onload = async function() {
     $('#videoDevice').append(videoDeviceHTML);
 };
 
+//Ask the users for permission to access video and audio devices
+async function getPermission() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+    //Start and immediately stop the stream
+    .then(function(stream) {
+        stream.getTracks()[0].stop();
+        stream.getTracks()[1].stop(); 
+    })
+    .catch(function(_err) {
+        //Error
+        alert("Couldn't get permission to access camera");
+    });
+}
 
+//Get the list of connected video devices to display to the user
 async function getVideoDevices() {
     const devices = await navigator.mediaDevices.enumerateDevices();
     return devices.filter(device => device.kind == 'videoinput')
