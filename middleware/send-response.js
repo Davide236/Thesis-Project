a8d7379f780a66962adb4439aedc7c329152d8c5
Davide236 <rigodavi99@gmail.com>
2022-06-18 16:30:30 +0200

    API changes

--- middleware/send-response.js
+++ middleware/send-response.js
@@ -4,13 +4,14 @@ module.exports.process_response = (req, res, response, redirect, render) => {
     switch(response.code) {
         case '200':
             if (response.data) {
-                return res.render(render, response.data)
+                return res.status(200).render(render, response.data)
             } else {
                 req.flash('success', response.message);
-                return res.redirect(redirect);
+                return res.status(200).redirect(redirect);
             }
         default:
             req.flash('error',response.message);
-            res.redirect(redirect);
+            stat = Number(response.code);
+            return res.status(stat).redirect(redirect);
     }
 }
